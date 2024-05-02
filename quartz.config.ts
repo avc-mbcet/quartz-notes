4d030dab17a7a75fbd5072ac54ec5dc768ec4afe
Llama <forgithub@strayllama.slmail.me>
2024-05-03 01:14:49 +0530

    Quartz sync: May 3, 2024, 1:14 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,8 +20,8 @@ const config: QuartzConfig = {
 			fontOrigin: "googleFonts",
 			cdnCaching: true,
 			typography: {
-				header: "Sedan SC",
-				body: "Itim",
+				header: "Noto Serif",
+				body: "Noto Serif",
 				code: "JetBrains Mono",
 			},
 			colors: {
