254870df1a3e0ed08cda8ed87ca3dfa09757e0f5
Llama <forgithub@strayllama.slmail.me>
2024-05-03 00:58:09 +0530

    Quartz sync: May 3, 2024, 12:58 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,85 +7,97 @@ import * as Plugin from "./quartz/plugins"
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
 const config: QuartzConfig = {
-  configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    enableSPA: true,
-    enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
-    theme: {
-      fontOrigin: "googleFonts",
-      cdnCaching: true,
-      typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-        darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-        },
-      },
-    },
-  },
-  plugins: {
-    transformers: [
-      Plugin.FrontMatter(),
-      Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
-      }),
-      Plugin.Latex({ renderEngine: "katex" }),
-      Plugin.SyntaxHighlighting({
-        theme: {
-          light: "github-light",
-          dark: "github-dark",
-        },
-        keepBackground: false,
-      }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
-      Plugin.GitHubFlavoredMarkdown(),
-      Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
-      Plugin.Description(),
-    ],
-    filters: [Plugin.RemoveDrafts()],
-    emitters: [
-      Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
-      Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
-      Plugin.ContentIndex({
-        enableSiteMap: true,
-        enableRSS: true,
-      }),
-      Plugin.Assets(),
-      Plugin.Static(),
-      Plugin.NotFoundPage(),
-    ],
-  },
+	configuration: {
+		pageTitle: "Amal's Notes",
+		enableSPA: true,
+		enablePopovers: true,
+		analytics: null,
+		locale: "en-US",
+		baseUrl: "avc-mbcet.github.io/quartz-notes",
+		ignorePatterns: [],
+		defaultDateType: "created",
+		theme: {
+			fontOrigin: "googleFonts",
+			cdnCaching: true,
+			typography: {
+				header: "Sedan SC",
+				body: "Itim",
+				code: "JetBrains Mono",
+			},
+			colors: {
+				lightMode: {
+					light: "#ececec", // nord0
+					lightgray: "#d8d8d8", // nord1
+					gray: "#c5c5c5", // nord2
+					darkgray: "#808080", // nord3
+					dark: "#333333", // nord4
+					secondary: "#88c0d0", // nord8
+					tertiary: "#d8dee9", // nord12
+					highlight: "rgba(143, 159, 169, 0.15)",
+				},
+				darkMode: {
+					light: "#333333", // nord4
+					lightgray: "#4c566a", // nord5
+					gray: "#6684b8", // nord6
+					darkgray: "#a8c6d0", // nord7
+					dark: "#ececec", // nord0
+					secondary: "#88c0d0", // nord8
+					tertiary: "#d8dee9", // nord12
+					highlight: "rgba(143, 159, 169, 0.15)",
+				},
+
+			},
+		},
+	},
+	plugins: {
+		transformers: [
+			Plugin.FrontMatter(),
+			Plugin.CreatedModifiedDate({
+				priority: ["frontmatter", "filesystem"],
+			}),
+			Plugin.Latex({ renderEngine: "katex" }),
+			Plugin.SyntaxHighlighting({
+				theme: {
+					light: "nord",
+					dark: "nord",
+				},
+				keepBackground: false,
+			}),
+			// Plugin.GitHubFlavoredMarkdown(),
+			Plugin.ObsidianFlavoredMarkdown({
+				comments: false,
+				highlight: true,
+				wikilinks: false,
+				callouts: true,
+				mermaid: true,
+				parseTags: true,
+				parseArrows: false,
+				parseBlockReferences: true,
+				enableInHtmlEmbed: false,
+				enableYouTubeEmbed: true,
+				enableVideoEmbed: true,
+				enableCheckbox: true
+			}),
+			Plugin.TableOfContents(),
+			Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+			Plugin.Description(),
+		],
+		filters: [Plugin.RemoveDrafts()],
+		emitters: [
+			Plugin.AliasRedirects(),
+			Plugin.ComponentResources(),
+			Plugin.ContentPage(),
+			Plugin.FolderPage(),
+			Plugin.TagPage(),
+			Plugin.ContentIndex({
+				enableSiteMap: true,
+				enableRSS: true,
+			}),
+			Plugin.Assets(),
+			Plugin.Static(),
+			Plugin.NotFoundPage(),
+		],
+	},
 }
 
 export default config
